fde05de4c2c6999497439bc7220ef6a50546b45c
ataru <ataru4101826514@gmail.com>
2022-08-14 10:47:24 +0900

    CreateABWalls

--- src/main/java/com/testplugin/test/Worlds/CC/CreateRoom.java
+++ src/main/java/com/testplugin/test/Worlds/CC/CreateRoom.java
@@ -64,29 +64,61 @@ public class CreateRoom {
         System.out.println();
 
     }
-    //前後の壁
-    public void CreateWallsAB(int PlayerNum, World world, Location startPoint, Material material) {
-        System.out.println(startPoint);
-        startPoint.add(1,0,0);
-        for (int k=1; k<=this.WALL_NUM; k++) {
-            for (int j=1; j<=this.HEIGHT; j++) {
+//    //前後の壁
+//    public void CreateWallsAB(int PlayerNum, World world, Location startPoint, Material material) {
+//        startPoint.add(1, 0, 0);
+//        for (int l = 1; l <= PlayerNum; l++) {
+//            for (int k = 1; k <= this.WALL_NUM; k++) {
+//                for (int j = 1; j <= this.HEIGHT; j++) {
+//                    startPoint.add(0, 1, 0);
+//                    for (int i = 1; i <= this.WIDTH; i++) {
+//                        startPoint.add(0, 0, 1);
+//                        world.getBlockAt(startPoint).setType(material);
+//                    }
+//                    startPoint.add(0, 0, -this.WIDTH);
+//                    System.out.println(startPoint);
+//                }
+//                System.out.println("--------------------");
+//                startPoint.add(0, -this.WIDTH, 0);
+//                startPoint.add(this.WIDTH + 1, 0, 0);
+//                System.out.println(startPoint);
+//                System.out.println("--------------------");
+//            }
+//            startPoint.add(0,0,6);
+//            startPoint.add(-11,0,0);
+//        }
+//        startPoint.add(0,0,-6);
+//    }
+//前後の壁
+public void CreateWallsAB(int PlayerNum, World world, Location startPoint, Material material) {
+    System.out.println(startPoint);
+    startPoint.add(1,0,0);
+    for (int i=1; i <= PlayerNum; i++) {
+        for (int j=1; j <= this.WALL_NUM; j++) {
+            for (int l=1; l <= this.HEIGHT; l++) {
                 startPoint.add(0,1,0);
-                for (int i=1; i<= this.WIDTH; i++) {
+                for (int m=1; m<= this.WIDTH; m++) {
                     startPoint.add(0,0,1);
                     world.getBlockAt(startPoint).setType(material);
                 }
-                startPoint.add(0,0,-this.WIDTH);
+                startPoint.add(0,0, -(this.WIDTH));
+                System.out.println(startPoint);
             }
-            startPoint.add(0,-this.WIDTH,0);
+            System.out.println("--------------------");
+            startPoint.add(0,-this.HEIGHT,0);
             startPoint.add(this.WIDTH+1,0,0);
-//            System.out.println("----------------");
-//            System.out.println(startPoint);
-//            System.out.println("----------------");
+            System.out.println(startPoint);
+            System.out.println("--------------------");
         }
-        startPoint.add(-11, 0, 0);
+        startPoint.add(-10,0,0);
         System.out.println("初期地点"+startPoint);
-        System.out.println();
+        startPoint.add(0,0,this.WIDTH+1);
+        System.out.println("次の壁"+startPoint);
     }
+    startPoint.add(0,0,-(this.WIDTH+1));
+    System.out.println("初期地点"+startPoint);
+
+}
 
     //天井
     public void CreateCeilings(int PlayerNum, World world, Location startPoint, Material material) {
@@ -111,7 +143,7 @@ public class CreateRoom {
 
     }
     public void Create(int PlayerNum, World world, Location[] startPoint, Material material) {
-//          CreateFloors(PlayerNum, world, startPoint[0], material);
+          CreateFloors(PlayerNum, world, startPoint[0], material);
 //          CreateWallsRL(PlayerNum, world, startPoint[1], material);
           CreateWallsAB(PlayerNum, world, startPoint[2], material);
 //          CreateCeilings(PlayerNum, world, startPoint[3], material);
